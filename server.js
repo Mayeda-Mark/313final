fb953f912f738162e2577b400d6cd18b49b3bd6a
Mayeda-Mark <markrmayeda@gmail.com>
2018-11-27 17:28:17 -0800

    working on stuff

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 
 var express = require("express");
 var app = express();
+const { Pool } = require("pg");
+const connectionString = process.env.DATABASE_URL || "postgress://footballuser:gohawks@localhost:5432/football";
+const pool = new Pool({connectionString: connectionString});
+
 const PORT = process.env.PORT || 8888
 app.use(express.static("public"));
 app.set("views", "views");
@@ -14,43 +18,40 @@ app.get("/", function(req, res) {
 	res.end();
 });
 
+app.get("/Football", function(req, res){
+	console.log("Connected to Football.ejs")
+	var bla = getWeeks(function(error, result){
+		console.log(result);
+		return result;
+	});
+	console.log(bla);
+	res.render("Football");
+})
+
 //price
-app.get("/price", function(req, res) {
+/*app.get("/price", function(req, res) {
 	var weight = parseFloat(req.param('weight'));
 	var type = parseInt(req.param('type'));
 	var price = getPrices(weight, type);
 	var param = { price: price };
 	res.render("price", param);
-})
+})*/
 
-function getPrices(weight, type) {
-	if ((type == 1 || 2) && (weight > 3.5)) {
-		return "Your parcel/letter is too heavy to send!";
-	}
-	if ((type == 1 || 2) && (weight < 3.5 && weight > 3)) {
-		weight = 4
-	}
-	if (weight > 13) {
-		return "Your parcel/letter is too heavy to send!";
-	}
-	var cost;
-	switch (type) {
-		case 1:
-			cost = (.50 + (parseInt(weight) * .21));
-			break;
-		case 2:
-			cost = (.47 + (parseInt(weight) * .21));
-			break;
-		case 3:
-			cost = (1 + (parseInt(weight) * .21));
-			break;
-		case 4:
-			cost = (3.5 + (parseInt(weight) * .21));
-			break;
-	}
-	return "Your parcel/letter costs $" + cost.toFixed(2);
+function getWeeks(callback){
+	console.log("getWeeks called");
+	var sql = "SELECT * FROM Week ORDER BY id;";
+	pool.query(sql, function(err, result){
+		if(err) {
+			console.log("An error with the database has occurred");
+			console.log(err);
+			callback(err, null);
+		}
+		//console.log("Found DB result: " + JSON.stringify(result.rows));
+		callback(null, result.rows);
+	});
 }
 
 app.listen(PORT, function() {
 	console.log("Up and running")
 })
+
