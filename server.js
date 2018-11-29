4d3c17eae1ef91ef15f9caaabc77cd10f6d70f80
Mayeda-Mark <markrmayeda@gmail.com>
2018-11-28 20:15:54 -0800

    worked on the project

--- server.js
+++ server.js
@@ -20,14 +20,24 @@ app.get("/", function(req, res) {
 
 app.get("/Football", function(req, res){
 	console.log("Connected to Football.ejs")
-	var bla = getWeeks(function(error, result){
-		console.log(result);
-		return result;
+	getWeeks(function(error, result){
+		getTeams(function(error, result2){
+		res.render("Football", {week: result, team: result2});
+		});
 	});
-	console.log(bla);
-	res.render("Football");
 })
 
+app.get("/results", function(req, res){
+	console.log("requesting table");
+	var id = req.query.Team1;
+	getTeamName(id, function(error, result){
+		console.log(result.name);
+		displayData(id, function(error, result2){
+		//console.log(result2);
+		res.render("results", {team: result, table: result2});
+		});
+	});
+});
 //price
 /*app.get("/price", function(req, res) {
 	var weight = parseFloat(req.param('weight'));
@@ -46,7 +56,18 @@ function getWeeks(callback){
 			console.log(err);
 			callback(err, null);
 		}
-		//console.log("Found DB result: " + JSON.stringify(result.rows));
+		callback(null, result.rows);
+	});
+}
+
+function getTeams(callback){
+	var sql = "SELECT * FROM Team ORDER BY name;";
+	pool.query(sql, function(err, result){
+		if(err) {
+			console.log("An error with the database has occurred");
+			console.log(err);
+			callback(err, null);
+		}
 		callback(null, result.rows);
 	});
 }
@@ -55,3 +76,26 @@ app.listen(PORT, function() {
 	console.log("Up and running")
 })
 
+function displayData(id, callback){
+	var sql = "SELECT Analysis.Team_id, Analysis.Week_id, Score.teamScore, Score.oppScore, Score.iswin, Spread.proj_spread, Score.realSpread, Analysis.spreadDifference FROM ((Analysis INNER JOIN Spread ON Analysis.spread_id = Spread.id) INNER JOIN Score ON Analysis.score_id = Score.id) WHERE Analysis.Team_id = " + id + " ORDER BY Analysis.Week_id;";
+	pool.query(sql, function(err, result){
+		if(err) {
+			console.log("An error with the database has occurred");
+			console.log(err);
+			callback(err, null);
+		}
+		callback(null, result.rows);
+	});
+}
+
+function getTeamName(id, callback){
+	var sql = "SELECT name FROM Team WHERE id = " + id + ";";
+	pool.query(sql, function(err, result){
+		if(err) {
+			console.log("An error with the database has occurred");
+			console.log(err);
+			callback(err, null);
+		}
+		callback(null, result.rows);
+	});
+}
